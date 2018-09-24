998c31fe199944313e605c62ca6b7cb6882cb7cb
pengtao <peengtao123@126.com>
2018-09-24 10:34:51 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  base:'/',
+  title:'学习笔记',
+  description:'日常学习积累',
+  themeConfig: {
+    sidebarDepth:1,
+    sidebar: [
+      ['/','首页'],
+      '/guide/',
+      '/config/'
+    ],
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '指南', link: '/guide/' },
+      { text: '配置', link: '/config/' },
+      { text: '默认主题配置', link: '/default-theme-config/' },
+      { text: '外部', link: 'https://google.com' },
+    ]
+  }
+}
